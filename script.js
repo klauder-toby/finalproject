a79f9f1964207fa54627dfe3dfbe91f509a85600
klauder-toby <tklauder21@georgefox.edu>
2021-11-16 23:19:34 -0800

    go go go

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 var url = "https://www.carboninterface.com/api/v1/estimates";
-var token = config.API_KEY; 
 
 
-var xhr = new XMLHttpRequest();
-xhr.open("POST", url);
+function submitRequest() {
+   var xhr = new XMLHttpRequest();
+   xhr.open("POST", url);
 
-xhr.setRequestHeader("Authorization", "Bearer " + token);
-xhr.setRequestHeader("Content-Type", "application/json");
-xhr.setRequestHeader("Content-Length", "0");
+   xhr.setRequestHeader("Authorization", "Bearer 7ZnGZ14gHXFw9RnZG4E1jA");
+   xhr.setRequestHeader("Content-Type", "application/json");
+   xhr.setRequestHeader("Content-Length", "0");
 
-xhr.onreadystatechange = function () {
-   if (xhr.readyState === 4) {
-      console.log(xhr.status);
-      console.log(xhr.responseText);
-   }};
+   xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+         console.log(xhr.status);
+         console.log(xhr.responseText);
+      }};
 
-xhr.send(JSON.stringify({}));
+   xhr.send(JSON.stringify({}));
+}
